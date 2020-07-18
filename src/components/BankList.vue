a446072991edd94bf7454c2afe5941fbc9cfecb9
Sowmiya Nagarajan <sowmiya_n@intuit.com>
2020-07-18 22:36:50 +0530

    Adding bank component

---
+++ src/components/BankList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="banklist">
+    <ul class="hs full">
+      <div class="item-one">
+        <h2>
+          Select
+          <br />Accounts
+        </h2>
+      </div>
+      <Bank v-for="bank in banks" :bank="bank" :key="bank.id" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Bank from '@/components/Bank'
+export default {
+  name: 'BankList',
+  components: {
+    Bank
+  },
+  data () {
+    return {
+      banks: [
+        {
+          id: 1,
+          name: 'Citibank',
+          imgName: 'citi.png',
+          accType: 'Savings',
+          accNo: 4545
+        },
+        {
+          id: 2,
+          name: 'HDFC Bank',
+          imgName: 'hdfc.png',
+          accType: 'Loan',
+          accNo: 3455
+        },
+        {
+          id: 3,
+          name: 'Axis Bank',
+          imgName: 'axis.png',
+          accType: 'Savings',
+          accNo: 9545
+        }
+      ]
+    }
+  }
+}
+</script>
